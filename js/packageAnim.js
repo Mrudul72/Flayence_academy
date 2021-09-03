d8858418070f5b6ffc8f9f006796fc05e1d37c74
Mrudul A Thakadiyel <mrudulathakadiyel72@gmail.com>
2021-09-03 20:54:04 +0530

    Package section animation added

--- js/packageAnim.js
+++ js/packageAnim.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-    $("#exclusive-pkg").on("tap",function(){
-        $('#pkg-sec').addClass("pkg-ex");
-      });
-  $('#pro-pkg').click(function(){
-    $('#pkg-sec').removeClass("pkg-ex");
-});
-  });
+const pkgSlide = () => {
+  const exclusive = document.querySelector("#exclusive-pkg");
+  const pro = document.querySelector("#pro-pkg");
+  const pkg = document.querySelector("#pkg-sec");
+
+  if (window.innerWidth <= 480) {
+    exclusive.addEventListener("click", () => {
+        pkg.classList.add("pkg-ex");
+    });
+    pro.addEventListener("click", () => {
+        pkg.classList.remove("pkg-ex");
+    });
+  }
+};
+
+pkgSlide();
