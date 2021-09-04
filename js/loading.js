583cb0be4f8d9f465b72fba0e570cb21982f7c47
Mrudul A Thakadiyel <mrudulathakadiyel72@gmail.com>
2021-09-04 10:12:49 +0530

    Package animation fixed

--- js/loading.js
+++ js/loading.js
@@ -1,13 +1,42 @@
+const main = document.querySelector(".main");
 document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector(
-          "body").style.visibility = "hidden";
-        document.querySelector(
-          "#loader").style.visibility = "visible";
-    } else {
-        document.querySelector(
-          "#loader").style.display = "none";
-        document.querySelector(
-          "body").style.visibility = "visible";
-    }
+  
+  if (document.readyState !== "complete") {
+    document.querySelector(
+      "body").style.visibility = "hidden";
+    document.querySelector(
+      "#loader").style.visibility = "visible";
+      disableScroll();
+
+      main.addEventListener(
+        "touchmove",
+        (main) => {
+          main.preventDefault();
+        },
+        false
+      );
+      
+
+} else {
+    document.querySelector(
+      "#loader").style.display = "none";
+    document.querySelector(
+      "body").style.visibility = "visible";
+      enableScroll();
+}
 };
+
+function disableScroll() {
+// Get the current page scroll position
+scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+  // if any scroll is attempted, set this to the previous value
+  window.onscroll = function() {
+      window.scrollTo(scrollLeft, scrollTop);
+  };
+}
+
+function enableScroll() {
+window.onscroll = function() {};
+}
--- js/packageAnim.js
+++ js/packageAnim.js
@@ -1,3 +1,11 @@
+const exclusive = document.querySelector("#exclusive-pkg");
+const pro = document.querySelector("#pro-pkg");
+const pkg = document.querySelector("#pkg-sec");
+const proTick = document.querySelectorAll(".pro-tick");
+const excTick = document.querySelectorAll(".exc-tick");
+const proIco = document.querySelector("#pro-ico");
+const excIco = document.querySelector("#exc-ico");
+
 const pkgSlide = () => {
   const exclusive = document.querySelector("#exclusive-pkg");
   const pro = document.querySelector("#pro-pkg");
@@ -5,12 +13,63 @@ const pkgSlide = () => {
 
   if (window.innerWidth <= 480) {
     exclusive.addEventListener("click", () => {
-        pkg.classList.add("pkg-ex");
+      pkg.classList.add("pkg-ex");
+      exclusive.classList.add("active-pkg");
+      pro.classList.remove("active-pkg");
+      excIco.src = "./assets/images/exclusive-ico-light.svg";
+      proIco.src = "./assets/images/pro-ico-dark.svg";
+      for (eachTick of excTick) {
+        eachTick.src = "./assets/images/tick-light.svg";
+      }
+      for (eachTick of proTick) {
+        eachTick.src = "./assets/images/tick.svg";
+      }
     });
     pro.addEventListener("click", () => {
-        pkg.classList.remove("pkg-ex");
+      pkg.classList.remove("pkg-ex");
+      pro.classList.add("active-pkg");
+      exclusive.classList.remove("active-pkg");
+      proIco.src = "./assets/images/pro-ico.svg";
+      excIco.src = "./assets/images/exclusive-ico.svg";
+      for (eachTick of excTick) {
+        eachTick.src = "./assets/images/tick.svg";
+      }
+      for (eachTick of proTick) {
+        eachTick.src = "./assets/images/tick-light.svg";
+      }
     });
   }
 };
 
 pkgSlide();
+
+const pkgActive = () => {
+  if (window.innerWidth > 480) {
+    exclusive.addEventListener("click", () => {
+      exclusive.classList.add("active-pkg");
+      pro.classList.remove("active-pkg");
+      excIco.src = "./assets/images/exclusive-ico-light.svg";
+      proIco.src = "./assets/images/pro-ico-dark.svg";
+      for (eachTick of excTick) {
+        eachTick.src = "./assets/images/tick-light.svg";
+      }
+      for (eachTick of proTick) {
+        eachTick.src = "./assets/images/tick.svg";
+      }
+    });
+    pro.addEventListener("click", () => {
+      pro.classList.add("active-pkg");
+      exclusive.classList.remove("active-pkg");
+      proIco.src = "./assets/images/pro-ico.svg";
+      excIco.src = "./assets/images/exclusive-ico.svg";
+      for (eachTick of excTick) {
+        eachTick.src = "./assets/images/tick.svg";
+      }
+      for (eachTick of proTick) {
+        eachTick.src = "./assets/images/tick-light.svg";
+      }
+    });
+  }
+};
+
+pkgActive();
