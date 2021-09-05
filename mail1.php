5f79ab2ab6c62da01a3e315bf47af539e989e636
Mrudul A Thakadiyel <mrudulathakadiyel72@gmail.com>
2021-09-05 21:47:36 +0530

    Font size changed and nav link scroll fix

--- mail1.php
+++ mail1.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])){
 
     if($uploadStatus == 1){
         // Recipient
-        $toEmail = "mrudulathakadiyel@mca.ajce.in";
+        $toEmail = "flayence@gmail.com";
         // Sender
         $from = 'info@flayence.com';
         $fromName = 'Flayence Academy';
